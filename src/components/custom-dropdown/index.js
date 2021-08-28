fcbd39ed6e508c628cc021f2d3fb3873007dac9f
Albertino Augusto <albertinoaugusto.it@gmail.com>
2021-08-28 18:32:30 +0200

    Update index.js

--- src/components/custom-dropdown/index.js
+++ src/components/custom-dropdown/index.js
@@ -16,9 +16,9 @@ import Icon from "@material-ui/core/Icon";
 import Popper from "@material-ui/core/Popper";
 
 // core components
-import Button from "components/CustomButtons/Button.js";
+import Button from "components/Button.js";
 
-import styles from "styles/components/customDropdownStyle.js";
+import styles from "styles/components/custom-dropdown.js";
 
 const useStyles = makeStyles(styles);
 
