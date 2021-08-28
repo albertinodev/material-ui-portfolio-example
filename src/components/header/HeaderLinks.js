bc7c49af1aac7c24bfcab2e517754f96633c2f75
Albertino Augusto <albertinoaugusto.it@gmail.com>
2021-08-28 18:36:01 +0200

    Update HeaderLinks.js

--- src/components/header/HeaderLinks.js
+++ src/components/header/HeaderLinks.js
@@ -15,8 +15,8 @@ import Tooltip from "@material-ui/core/Tooltip";
 import { Apps, CloudDownload } from "@material-ui/icons";
 
 // core components
-import CustomDropdown from "components/CustomDropdown/CustomDropdown.js";
-import Button from "components/CustomButtons/Button.js";
+import CustomDropdown from "components/custom-dropdown";
+import Button from "components/Button.js";
 
 import styles from "styles/components/headerLinksStyle.js";
 
