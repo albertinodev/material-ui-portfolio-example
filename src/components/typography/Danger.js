f6fd2293cf7b866a478eec958a2da2c84f1aef0a
Albertino Augusto <albertinoaugusto.it@gmail.com>
2021-08-28 18:37:54 +0200

    Update Danger.js

--- src/components/typography/Danger.js
+++ src/components/typography/Danger.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
 // core components
-import styles from "styles/components/typographyStyle.js";
+import styles from "styles/components/typography.js";
 
 const useStyles = makeStyles(styles);
 
