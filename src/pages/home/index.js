d63c0a63b43d54bd7c2e15a2f7f81359b5a2a76d
Albertino Augusto <albertinoaugusto.it@gmail.com>
2021-08-28 18:58:29 +0200

    Update index.js

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -7,13 +7,13 @@ import { makeStyles } from "@material-ui/core/styles";
 // @material-ui/icons
 
 // core components
-import Header from "components/Header/Header.js";
-import Footer from "components/Footer/Footer.js";
-import GridContainer from "components/Grid/GridContainer.js";
-import GridItem from "components/Grid/GridItem.js";
-import Button from "components/CustomButtons/Button.js";
-import HeaderLinks from "components/Header/HeaderLinks.js";
-import Parallax from "components/Parallax/Parallax.js";
+import Header from "components/header/Header.js";
+import Footer from "components/footer/Footer.js";
+import GridContainer from "components/grid/GridContainer.js";
+import GridItem from "components/grid/GridItem.js";
+import Button from "components/customButtons/Button.js";
+import HeaderLinks from "components/header/HeaderLinks.js";
+import Parallax from "components/parallax/Parallax.js";
 
 import styles from "assets/jss/material-kit-react/views/home.js";
 
